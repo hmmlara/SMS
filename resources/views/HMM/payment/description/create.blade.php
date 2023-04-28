4cad1cceb3f345ccef6d2b64a2d14b308db8212c
Pyae Phyo Zan <pyae418283@gmail.com>
2023-04-28 23:46:11 +0630

    update code

--- resources/views/HMM/payment/description/create.blade.php
+++ resources/views/HMM/payment/description/create.blade.php
@@ -14,34 +14,77 @@
         <div class="card-content">
             <h4 class="mb-3"><strong>New Description</strong></h4>
         </div>
-        <div class="row">
-            <div class="form-group d-flex">
-                <label for="" class="form-label me-2">Title: </label>
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="row mt-4 mb-4">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="col-6 mb-3">
+                <div class="form-group d-flex">
+                    <label for="" class="form-label me-2">Title: </label>
+                    <div class="input-gp w-100">
+                        <input type="text" name="title" id="" class="form-control" style="height: 30px;"
+                            placeholder="Enter title">
+                        <small class="text-danger" id="error-title"></small>
+                    </div>
+                </div>
+            </div>
 
-                <div class="input-gp w-100">
-                    <input type="text" name="title" id="" class="form-control w-50" style="height: 30px;"
-                        placeholder="Enter title">
-                    <small class="text-danger" id="error-msg"></small>
+            <div class="col-6 mb-3">
+                <div class="form-group d-flex">
+                    <label for="" class="form-label me-2">Unit: </label>
+                    <div class="input-gp w-100">
+                        <input type="text" name="units" id="" class="form-control" style="height: 30px;"
+                            placeholder="Enter Unit">
+                        <small class="text-danger" id="error-units"></small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 mb-3">
+                <div class="form-group d-flex">
+                    <label for="" class="form-label me-2">Cost: </label>
+                    <div class="input-gp w-100">
+                        <input type="text" name="cost" id="" class="form-control" style="height: 30px;"
+                            placeholder="Enter Cost">
+                        <small class="text-danger ml-4" id="error-cost"></small>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
     <script>
-        $('input[name="title"]').on('focus',function(){
-            $('input[name="title"]').removeClass('form-control border border-danger');
+        const titleInput = $('input[name="title"]');
+        const unitInput = $('input[name="units"]');
+        const costInput = $('input[name="cost"]');
+
+        titleInput.on('focus', function() {
+            $(this).removeClass('form-control border border-danger');
             $(this).addClass('form-control');
-            $('#error-msg').text("");
+            $('#error-title').text("");
+        });
+
+        unitInput.on('focus', function() {
+            $(this).removeClass('form-control border border-danger');
+            $(this).addClass('form-control');
+            $('#error-units').text("");
+        });
+
+        costInput.on('focus', function() {
+            $(this).removeClass('form-control border border-danger');
+            $(this).addClass('form-control');
+            $('#error-cost').text("");
         });
 
         $('#save').on('click', function() {
-            let title = $('input[name="title"]').val();
+            let title = titleInput.val();
+            let units = unitInput.val();
+            let cost = costInput.val();
             let _token = $('input[name="_token"]').val();
 
             let data = {
                 _token: _token,
-                "title": title
+                "title": title,
+                "units": units,
+                "cost": cost,
             };
             // console.log(data);
             $.ajax({
@@ -54,8 +97,20 @@
                     }
                 },
                 error: function(response) {
-                    $('input[name="title"]').addClass('form-control border border-danger');
-                    $('#error-msg').text(response.responseJSON.errors.title);
+                    let data = response.responseJSON.errors;
+
+                    if (data.hasOwnProperty('title')) {
+                        titleInput.addClass('form-control border border-danger');
+                        $('#error-title').text(response.responseJSON.errors.title);
+                    }
+                    if (data.hasOwnProperty('units')) {
+                        unitInput.addClass('form-control border border-danger');
+                        $('#error-units').text(response.responseJSON.errors.units);
+                    }
+                    if (data.hasOwnProperty('cost')) {
+                        costInput.addClass('form-control border border-danger');
+                        $('#error-cost').text(response.responseJSON.errors.cost);
+                    }
                 }
             });
         });
