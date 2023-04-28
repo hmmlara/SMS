1a5f74876f0d615dbf897e9768e30648aaa3bb32
Pyae Phyo Zan <pyae418283@gmail.com>
2023-04-28 23:47:30 +0630

    add resource route

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::group(['prefix' => 'hmm-group', 'namespace' => 'App\Http\Controllers'], function () {
 
